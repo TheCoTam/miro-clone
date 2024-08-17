7a7e5e7f5a96e0533f8ad459fd6fdfe2da12110d
TheCoTam <hoangnguyenanhtuyen@gmail.com>
2024-08-17 22:51:54 +0700

    giao diện khi không có board

---
+++ app/(dashboard)/_components/board-list.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import EmptyBoard from "./empty-board";
+import EmptyFavorites from "./empty-favorites";
+import EmptySearch from "./empty-search";
+
+interface BoardListProps {
+  orgId: string;
+  query: {
+    search?: string;
+    favorites?: string;
+  };
+}
+
+const BoardList = ({ orgId, query }: BoardListProps) => {
+  const data = [];
+
+  if (!data?.length && query.search) {
+    return <EmptySearch />;
+  }
+
+  if (!data?.length && query.favorites) {
+    return <EmptyFavorites />;
+  }
+
+  if (!data?.length) {
+    return <EmptyBoard />;
+
+    return (
+      <div>
+        <div>{orgId}</div>
+        <div>{JSON.stringify(query)}</div>
+      </div>
+    );
+  }
+};
+export default BoardList;
