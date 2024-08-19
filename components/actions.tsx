ce0b7811667d335ca0db6ce9ade2315e4fcd3673
TheCoTam <hoangnguyenanhtuyen@gmail.com>
2024-08-19 16:31:33 +0700

    thêm hành động xoá board

--- components/actions.tsx
+++ components/actions.tsx
@@ -1,15 +1,19 @@
 "use client";
 
-import { Link2 } from "lucide-react";
+import { Link2, Trash2 } from "lucide-react";
 import { toast } from "sonner";
 import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
 
+import { api } from "@/convex/_generated/api";
+import { useApiMutation } from "@/hooks/use-api-mutation";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import ConfirmModal from "@/components/confirm-modal";
+import { Button } from "@/components/ui/button";
 
 interface ActionsProps {
   children: React.ReactNode;
@@ -20,11 +24,19 @@ interface ActionsProps {
 }
 
 const Actions = ({ children, side, sideOffset, id, title }: ActionsProps) => {
+  const { mutate, pending } = useApiMutation(api.board.remove);
+
   const onCopyLink = () => {
     navigator.clipboard
       .writeText(`${window.location.origin}/board/${id}`)
       .then(() => toast.success("Link copied"))
       .catch(() => toast.error("Failed to copy"));
+  };
+
+  const onDelete = () => {
+    mutate({ id })
+      .then(() => toast.success("Board deleted"))
+      .catch(() => toast.error("Failed to delete board"));
   };
 
   return (
@@ -40,6 +52,20 @@ const Actions = ({ children, side, sideOffset, id, title }: ActionsProps) => {
           <Link2 className="w-4 h-4 mr-2" />
           Copy board link
         </DropdownMenuItem>
+        <ConfirmModal
+          header="Delete board?"
+          description="This will delete the board and all of its contents."
+          disabled={pending}
+          onConfirm={onDelete}
+        >
+          <Button
+            variant="ghost"
+            className="p-3 cursor-pointer text-sm w-full justify-start font-normal"
+          >
+            <Trash2 className="w-4 h-4 mr-2" />
+            Delete
+          </Button>
+        </ConfirmModal>
       </DropdownMenuContent>
     </DropdownMenu>
   );
