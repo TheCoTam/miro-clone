24f1c72446761b80a1ad540c86a1c2a9e6b4de2a
TheCoTam <hoangnguyenanhtuyen@gmail.com>
2024-08-19 15:45:31 +0700

    viết api lấy toàn bộ board của organization hiện tại và thêm 1 board mới

--- convex/board.ts
+++ convex/board.ts
@@ -2,7 +2,16 @@ import { v } from "convex/values";
 
 import { mutation } from "./_generated/server";
 
-const images = ["htihsad", "akjshdkasdjkasdkj"];
+const images = [
+  "/jeep.gif",
+  "/balloons.gif",
+  "/balloons_2.gif",
+  "/balloons_3.gif",
+  "/valentines-day.gif",
+  "/baseball.gif",
+  "/interaction.gif",
+  "/lighthouse.gif",
+];
 
 export const create = mutation({
   args: {
