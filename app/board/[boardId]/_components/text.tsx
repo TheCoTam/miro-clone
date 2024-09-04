d1e7b0344b6b39569379103b620930191bd745a1
TheCoTam <hoangnguyenanhtuyen@gmail.com>
2024-09-04 22:30:32 +0700

    hoàn thiện hành động thêm 1 đoạn chữ

---
+++ app/board/[boardId]/_components/text.tsx
@@ -0,0 +1,61 @@
+import { useMutation } from "@liveblocks/react/suspense";
+import ContentEditable, { ContentEditableEvent } from "react-contenteditable";
+
+import { TextLayer } from "@/types/canvas";
+
+import { colorToCss } from "@/lib/utils";
+
+interface TextProps {
+  id: string;
+  layer: TextLayer;
+  onPointerDown: (e: React.PointerEvent, id: string) => void;
+  selectionColor?: string;
+}
+
+const calcFontSize = (width: number, height: number) => {
+  const maxFontSize = 96;
+  const scaleFactor = 0.5;
+  const fontSizeBasedOnHeight = height * scaleFactor;
+  const fontSizeBasedOnWidth = width * scaleFactor;
+
+  return Math.min(fontSizeBasedOnHeight, fontSizeBasedOnWidth, maxFontSize);
+};
+
+const Text = ({ id, layer, onPointerDown, selectionColor }: TextProps) => {
+  const { x, y, width, height, fill, value } = layer;
+
+  const updateValue = useMutation(({ storage }, newValue: string) => {
+    const liveLayers = storage.get("layers");
+
+    liveLayers.get(id)?.set("value", newValue);
+  }, []);
+
+  const handleContentChange = (e: ContentEditableEvent) => {
+    updateValue(e.target.value);
+  };
+
+  return (
+    <foreignObject
+      x={x}
+      y={y}
+      width={width}
+      height={height}
+      onPointerDown={(e) => onPointerDown(e, id)}
+      style={{
+        outline: selectionColor ? `1px solid ${selectionColor}` : "none",
+      }}
+    >
+      <ContentEditable
+        html={value || "Text"}
+        onChange={handleContentChange}
+        className="h-full w-full flex items-center justify-center text-center drop-shadow-md outline-none"
+        style={{
+          color: fill ? colorToCss(fill) : "#000",
+          fontSize: calcFontSize(width, height),
+        }}
+      />
+    </foreignObject>
+  );
+};
+
+export default Text;
