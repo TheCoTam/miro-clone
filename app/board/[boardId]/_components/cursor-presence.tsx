cafd918ba7165a981c83ad6fbf911eb91c77e6a6
TheCoTam <hoangnguyenanhtuyen@gmail.com>
2024-09-08 20:02:39 +0700

    hiển thị khi người dùng khác đang vẽ

--- app/board/[boardId]/_components/cursor-presence.tsx
+++ app/board/[boardId]/_components/cursor-presence.tsx
@@ -1,9 +1,15 @@
 "use client";
 
-import { useOthersConnectionIds } from "@liveblocks/react/suspense";
+import {
+  shallow,
+  useOthersConnectionIds,
+  useOthersMapped,
+} from "@liveblocks/react/suspense";
 import { memo } from "react";
 
 import Cursor from "./cursor";
+import Path from "./path";
+import { colorToCss } from "@/lib/utils";
 
 const Cursors = () => {
   const ids = useOthersConnectionIds();
@@ -17,9 +23,40 @@ const Cursors = () => {
   );
 };
 
+const Drafts = () => {
+  const other = useOthersMapped(
+    (other) => ({
+      pencilDraft: other.presence.pencilDraft,
+      penColor: other.presence.penColor,
+    }),
+    shallow
+  );
+
+  return (
+    <>
+      {other.map(([key, other]) => {
+        if (other.pencilDraft) {
+          return (
+            <Path
+              key={key}
+              x={0}
+              y={0}
+              points={other.pencilDraft}
+              fill={other.penColor ? colorToCss(other.penColor) : "#000"}
+            />
+          );
+        }
+
+        return null;
+      })}
+    </>
+  );
+};
+
 export const CursorPresence = memo(() => {
   return (
     <>
+      <Drafts />
       <Cursors />
     </>
   );
